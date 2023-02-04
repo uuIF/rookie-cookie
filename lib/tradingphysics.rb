a01618b1724e68aa530f7ddab5669492c89a5915
uuIF <107625692+uuIF@users.noreply.github.com>
2023-02-04 17:27:30 +0000

    Fixing some typos.

--- lib/tradingphysics.rb
+++ lib/tradingphysics.rb
@@ -6,3 +6,5 @@ module Tradingphysics
   #Used for getting data from the server
   class TradingPhysics
     # Construct the trading physics class with the customer_number and hash	
+    def initialize(customer_number, hash)
+      @customer_number=customer_number
